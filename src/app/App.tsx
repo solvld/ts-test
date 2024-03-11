d5a132d7dd5c95f5d3f31f77e4b5ccc3978df9c3
solvld <sol.vld@yandex.ru>
2024-03-11 22:02:11 +0400

    fix linter rules for prettier

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-
 import { useEffect } from 'react'
 import { outputImage } from '../learnTS/PixelSmile'
 // import CharForm from '../components/CharForm'
 import ArrivalTime from '../components/ArrivalTime'
 
 const App = () => {
-
   useEffect(() => {
     outputImage()
-  },[])
+  }, [])
   return (
     <>
       <ArrivalTime />
+      <div>k</div>
     </>
   )
 }
